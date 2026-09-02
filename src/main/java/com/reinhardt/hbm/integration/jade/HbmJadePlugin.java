0c570a7bc1bd83f08ec8d2ac09bff872c517fddc
Reinhardt <sakurakoujisaika@outlook.com>
2026-09-02 12:10:13 +0800

    Add Jade dummy machine tooltip integration

---
+++ src/main/java/com/reinhardt/hbm/integration/jade/HbmJadePlugin.java
@@ -0,0 +1,64 @@
+package com.reinhardt.hbm.integration.jade;
+
+import com.reinhardt.hbm.ReinhardtsHBM;
+import com.reinhardt.hbm.block.MachineDummyBlock;
+import com.reinhardt.hbm.blockentity.MachineDummyBlockEntity;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import snownee.jade.api.BlockAccessor;
+import snownee.jade.api.IBlockComponentProvider;
+import snownee.jade.api.IWailaClientRegistration;
+import snownee.jade.api.IWailaPlugin;
+import snownee.jade.api.ITooltip;
+import snownee.jade.api.JadeIds;
+import snownee.jade.api.WailaPlugin;
+import snownee.jade.api.config.IPluginConfig;
+
+@WailaPlugin
+public final class HbmJadePlugin implements IWailaPlugin {
+    @Override
+    public void registerClient(IWailaClientRegistration registration) {
+        registration.registerBlockComponent(MachineDummyProvider.INSTANCE, MachineDummyBlock.class);
+    }
+
+    private enum MachineDummyProvider implements IBlockComponentProvider {
+        INSTANCE;
+
+        @Override
+        public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
+            if (!(accessor.getBlockEntity() instanceof MachineDummyBlockEntity dummy)) {
+                return;
+            }
+
+            BlockEntity core = dummy.core();
+            if (core == null) {
+                return;
+            }
+
+            BlockState coreState = accessor.getLevel().getBlockState(core.getBlockPos());
+            if (coreState.isAir()) {
+                return;
+            }
+
+            // Jade already renders the core object name and all storage components.
+            // Replace only that name so dummy blocks identify the actual machine.
+            tooltip.replace(JadeIds.CORE_OBJECT_NAME, coreState.getBlock().getName());
+        }
+
+        @Override
+        public ResourceLocation getUid() {
+            return ReinhardtsHBM.id("jade_machine_dummy");
+        }
+
+        @Override
+        public int getDefaultPriority() {
+            return -10_090;
+        }
+
+        @Override
+        public boolean isRequired() {
+            return true;
+        }
+    }
+}
